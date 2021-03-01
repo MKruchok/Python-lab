7ff6d96825b618fdc1430844ba1b63d44797ad4f
MKruchok <74894852+MKruchok@users.noreply.github.com>
2021-03-02 01:56:22 +0200

    Lab 1 on IoT course in Lviv Polytechnic University (Python 2021)

---
+++ PytLab1.py
@@ -0,0 +1,36 @@
+class Couch:
+    best_couch = "Laura"
+    @staticmethod
+    def best__couch():
+        return Couch.best_couch
+
+    def __init__(self, name="Not specified", material="Not specified", in_stock="No", width: float=0, height: float=0, color="Not specified", brand="Not specified") :
+        self._name = name
+        self._material = material
+        self._in_stock = in_stock
+        self._width = width
+        self._height = height
+        self._color = color
+        self._brand = brand
+
+    def __del__(self):
+        print("\nObject deleted.")
+
+    def __str__(self):
+        value = ("Name: "+self._name+ "\nMaterial: "+self._material+ "\nIn_stock: "+self._in_stock+ "\nWidth: "+str(self._width) +"\nHeigh: "+str(self._height)+ "\nColor: "+self._color+ "\nBrand: "+self._brand+ "\n")
+        return value
+
+def main():
+    obj1 = Couch("Laura","Pirano","Yes",250,80,"white","Claudia")
+    print(obj1.__str__())
+
+    obj2 = Couch("Laura","Pirano","No",250,80,"black")
+    print(obj2.__str__())
+
+    obj3 = Couch("Laura","Pirano","Yes",250,80)
+    print(obj3.__str__())
+
+    print(f"\nBest couch: {obj1.best__couch()}")
+
+if 1:
+    main()
